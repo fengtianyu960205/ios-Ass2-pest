870f9865de5980e4e7e4ff924fe7e52d0e18187e
Philip <fyuu0010@student.monash.edu>
2020-11-06 01:20:26 +1100

    create coredata

---
+++ FengtianYu_JiayinQi-Ass2-ios/FengtianYu_JiayinQi-Ass2-ios/Data Model/UserCD+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  UserCD+CoreDataClass.swift
+//  
+//
+//  Created by 俞冯天 on 6/11/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(UserCD)
+public class UserCD: NSManagedObject {
+
+}
