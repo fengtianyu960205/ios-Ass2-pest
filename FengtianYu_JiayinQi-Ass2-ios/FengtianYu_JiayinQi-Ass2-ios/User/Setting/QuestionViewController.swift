13d257de262310c2b5014b1a48b2d0f2e46517ff
Philip <fyuu0010@student.monash.edu>
2020-11-21 02:51:59 +1100

    improve

---
+++ FengtianYu_JiayinQi-Ass2-ios/FengtianYu_JiayinQi-Ass2-ios/User/Setting/QuestionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QuestionViewController.swift
+//  FengtianYu_JiayinQi-Ass2-ios
+//
+//  Created by 俞冯天 on 21/11/20.
+//  Copyright © 2020 Monash University. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
